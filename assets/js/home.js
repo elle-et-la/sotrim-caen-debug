8e2b4cd14e9526afa0431352e6baa6b648bc42bd
Michael R <tekmedias@gmail.com>
2021-06-14 16:09:03 +0200

    :zap: build(core): initialize files

---
+++ assets/js/home.js
@@ -0,0 +1,109 @@
+let districtIframes = [];
+
+let carrouselData = {
+    current: 0,
+    images: false,
+    stop: false
+};
+
+$(document).ready(function(){
+    setHomeCarrousel();
+    setDistrictIframes();
+});
+
+let setHomeCarrousel = function(){
+    $.getJSON('./config/home-carrousel.json', function(data) {
+        carrouselData.images = data;
+
+        $('.homepage .presentation .navigation .item').hover(function(){
+            carrouselData.stop = true;
+        }, function(){
+            carrouselData.stop = false;
+        }).click(function(){
+            switchSlideCaroussel($(this).data('direction'), true);
+        });
+
+        setInterval(function(){
+            switchSlideCaroussel('next');
+        }, 7000);
+    })
+};
+
+let switchSlideCaroussel = function(direction, force){
+    if(!carrouselData.stop || force){
+        switch(direction){
+            case 'next':
+                carrouselData.current = (carrouselData.current + 1) <= (carrouselData.images.length - 1) ? carrouselData.current + 1 : 0;
+                break;
+            case 'prev':
+                carrouselData.current = (carrouselData.current - 1) >= 0 ? carrouselData.current - 1 : carrouselData.images.length - 1;
+                break;
+        }
+
+        let mainElem = $('.homepage .presentation .content-background');
+        let src = './assets/img/home/carrousel/' + carrouselData.images[carrouselData.current];
+
+        mainElem.css('backgroundPositionX', '1000px');
+        setTimeout(function() {
+            mainElem.css('backgroundImage', 'url("' + src + '")');
+            setTimeout(function () {
+                mainElem.css('backgroundPositionX', '50%');
+            }, 300);
+        }, 300);
+    }
+};
+
+let setDistrictIframes = function(){
+    $.getJSON('./config/district-iframes.json', function(data) {
+        let mainElem = $('#district-iframes');
+        let menuElem = mainElem.children('[data-menu]');
+        let contentIframesElem = mainElem.children('[data-iframes]');
+
+        $.each(data, function(index, iframe){
+            let menuItemElem = $('<div class="menu-item"></div>');
+            menuItemElem.html(iframe.name);
+            menuItemElem.click(function(){
+                showDistrictIframe(index);
+            });
+
+            let iframeElem = $('<div class="content-iframe" data-index="' + index + '"></div>');
+            let innerElem;
+            switch (iframe.type){
+                case "image":
+                    innerElem = $('<img>');
+                    innerElem.attr('src', iframe.url);
+                    break;
+                case "iframe":
+                    innerElem  = $('<iframe></iframe>');
+                    innerElem.attr('src', iframe.url);
+                    break;
+            }
+            innerElem.addClass('iframe-item');
+
+            if(index === 0){
+                menuItemElem.addClass('active');
+                iframeElem.addClass('active');
+            }
+
+            menuElem.append(menuItemElem);
+            iframeElem.append(innerElem);
+            contentIframesElem.append(iframeElem);
+            districtIframes.push({
+                menuItem: menuItemElem,
+                iframeItem: iframeElem,
+            });
+        });
+    });
+};
+
+let showDistrictIframe = function(index){
+    $.each(districtIframes, function(_index, item){
+        if(item.iframeItem.data('index') === index){
+            item.menuItem.addClass('active');
+            item.iframeItem.addClass('active')
+        }else{
+            item.menuItem.removeClass('active');
+            item.iframeItem.removeClass('active');
+        }
+    });
+};
---
+++ assets/js/script.js
@@ -0,0 +1,287 @@
+let residences = [];
+
+$(function(){
+    let includes = $('[data-include]');
+    $.each(includes, function () {
+        let file = 'views/' + $(this).data('include') + '.html';
+        $(this).load(file, function(){
+            switch($(this).data('include')){
+                case 'header':
+                    setMenuPositioning();
+                    setMainMenuNavigation($(this).data('selected'));
+                    break;
+                case 'residences':
+                    loadResidences();
+                    break;
+                case 'inscription':
+                    setGoToInformations();
+                    setInscriptionForm();
+                    break;
+                case 'footer':
+                    setFooterNavigation();
+                    setLegals();
+                    break;
+            }
+        });
+    });
+});
+
+let setMenuPositioning = function(selected){
+    let mainElem = $('.main-header');
+    let scrollPosition =  $(window).scrollTop();
+
+    $(window).scroll(function(){
+        let current = $(window).scrollTop();
+        if (current > scrollPosition){
+            if(current > 70){
+                mainElem.removeClass('active');
+            }else{
+                mainElem.addClass('active');
+            }
+        } else {
+            mainElem.addClass('active');
+        }
+        scrollPosition = current;
+    });
+};
+
+let setMainMenuNavigation = function(selected){
+    let mainElem = $('.main-header .menu .menu-item');
+
+    if(selected){
+        mainElem.each(function(){
+            if($(this).data('current') === selected){
+                $(this).addClass('current')
+            }
+        })
+    }
+    setResponsiveMenu();
+    setInscriptionResponsive();
+    setScrollNavigation();
+};
+
+let setGoToInformations = function(){
+    let mainElem = $('#goto-informations');
+    let inscriptionElem = $('[data-target="' + mainElem.data('scroll') + '"]');
+
+    if(inscriptionElem.length){
+        if(inscriptionElem.isInViewport()){
+            mainElem.removeClass('active')
+        }else{
+            mainElem.addClass('active')
+        }
+        $(window).on('scroll', function(){
+            if(inscriptionElem.isInViewport()){
+                mainElem.removeClass('active')
+            }else{
+                mainElem.addClass('active')
+            }
+        });
+    }
+};
+
+let setInscriptionForm = function(){
+    let mainElem = $('form.inscription-form');
+    mainElem.unbind('submit').submit(function(){
+        console.log('ok !', $(this));
+        return false;
+    });
+};
+
+let setResponsiveMenu = function(){
+    $('.main-header .responsive-menu .menu-item.open-menu').click(function(){
+        $(this).toggleClass('open');
+        $('.main-header .menu').toggleClass('open');
+        $('.main-header .responsive-menu .menu-item.mail').toggleClass('active');
+        return false;
+    });
+
+    $('.main-header .responsive-menu .menu-item.mail').click(function(){
+        $('.content-inscription').addClass('open');
+        return false;
+    });
+};
+
+let setInscriptionResponsive = function(){
+    $('.content-inscription .closer').unbind('click').click(function(){
+        $('.content-inscription').removeClass('open');
+    });
+};
+
+let setScrollNavigation = function(){
+    $('[data-scroll]').unbind('click').click(function(){
+        closeResidenceModal();
+
+        $('.main-header .menu').removeClass('open');
+        $('.main-header .responsive-menu .menu-item.open-menu').removeClass('open');
+        $('.main-header .responsive-menu .menu-item.mail').addClass('active');
+
+        let elem = $('[data-target="'+$(this).data('scroll')+'"]');
+        if(elem.length){
+            $('html, body').animate( { scrollTop: elem.offset().top - 100}, 500 );
+        }else{
+            window.location.href = "./";
+        }
+        return false;
+    });
+};
+
+let setFooterNavigation = function(){
+    $('.gotop').unbind("click").click(function(){
+        let test = false;
+        $.each(residences, function(index, residence){
+            if(residence.modal && residence.modal.hasClass('open')){
+                residence.modal.animate( { scrollTop: 0}, 500 );
+                test = true;
+            }
+        });
+        if(!test){
+            $('html').animate( { scrollTop: 0}, 500 );
+        }
+        return false;
+    });
+};
+
+let setLegals = function(){
+    $('.legals-opener').unbind("click").click(function(){
+        $('body').addClass('noscroll');
+        $('.content-legals').addClass('active');
+        return false;
+    });
+
+    $('.legals-closer').unbind("click").click(function(){
+        $('body').removeClass('noscroll');
+        $('.content-legals').removeClass('active');
+        return false;
+    });
+};
+
+let loadResidences = function(){
+    $.getJSON('./config/residences.json', function(data) {
+        setResidences(data);
+    });
+};
+
+let setResidences = function(data){
+    let mainElem = $('.residences-list');
+    let modalElem = $('#content-residences-modal').children('.residence-modal.default');
+
+    $.each(data, function(index, item){
+        let thumbnailElem = $('<div class="residence-thumbnail"></div>');
+        let src = "assets/img/residences/" + item.folderImg + '/' + item.thumbnail;
+        thumbnailElem.css('backgroundImage', 'url("' + src + '")');
+
+        let residenceElem = $('<div class="residence"></div>');
+        residenceElem.append(thumbnailElem);
+
+        if(item.active){
+            let nameElem = $('<div class="residence-name"></div>');
+            let pretitle = $('<div class="residence-pre-title">Résidence</div>');
+            let title = $('<h3 class="residence-title">' + item.name + '</h3>');
+            let opener = $('<div class="opener"></div>');
+
+            nameElem.append(pretitle).append(title).append(opener);
+            residenceElem.append(nameElem);
+
+            residenceElem.click(function(){
+                openResidenceModal(item.id);
+            });
+        }else{
+            let mentionElem = $('<div class="mention">Prochainement</div>');
+            residenceElem.prepend(mentionElem);
+            residenceElem.addClass('inactive');
+        }
+
+        residences.push({
+            elem: residenceElem,
+            item: item
+        });
+        mainElem.append(residenceElem);
+    });
+
+    $.each(residences, function(index, residence){
+        if(residence.item.active){
+            residence.modal = setResidenceModal(modalElem.clone(true), residence.item);
+        }
+    });
+};
+
+let setResidenceModal = function(defaultModal, residence){
+    let mainElem = $('#content-residences-modal');
+
+    defaultModal.data('id', residence.id);
+    defaultModal.removeClass('default');
+
+    let closerElem = defaultModal.children('[data-closer]');
+    let url = './assets/img/residences/' + residence.folderImg + '/' + residence.closer;
+    closerElem.css('backgroundImage', 'url("' + url + '")');
+    closerElem.click(function(){
+        closeResidenceModal();
+    });
+
+    let bodyElem = defaultModal.children('.residence-modal-body');
+    let file = 'views/' + residence.template + '.html';
+    bodyElem.load(file);
+
+    let includes = defaultModal.children('.residence-modal-footer').children('[data-include]');
+    $.each(includes, function () {
+        let file = 'views/' + $(this).data('include') + '.html';
+        $(this).load(file, function(){
+            switch($(this).data('include')){
+                case 'inscription':
+                    setInscriptionForm();
+                    setInscriptionResponsive();
+                    break;
+                case 'footer':
+                    setFooterNavigation();
+                    setScrollNavigation();
+                    setLegals();
+                    break;
+            }
+        });
+    });
+
+    mainElem.prepend(defaultModal);
+    return defaultModal;
+};
+
+let openResidenceModal = function(id){
+    $('body').addClass('noscroll');
+
+    $.each(residences, function(index, residence){
+        if(residence.item.active){
+            if(id === residence.item.id){
+                if(!residence.modal.hasClass('open')){
+                    residence.modal.scrollTop(0);
+                }else{
+                    residence.modal.animate( { scrollTop: 0}, 500 );
+                }
+                residence.modal.addClass('open');
+            }else{
+                residence.modal.removeClass('open');
+            }
+        }
+    });
+};
+
+let closeResidenceModal = function(){
+    $('body').removeClass('noscroll');
+
+    $.each(residences, function(index, residence){
+        if(residence.item.active){
+            residence.modal.removeClass('open');
+        }
+    });
+};
+
+$.fn.isInViewport = function() {
+    if($(this)){
+        let elementTop = $(this).offset().top;
+        let elementBottom = elementTop + $(this).outerHeight();
+
+        let viewportTop = $(window).scrollTop();
+        let viewportBottom = viewportTop + $(window).height();
+
+        return elementBottom > viewportTop && elementTop < viewportBottom;
+    }
+};
