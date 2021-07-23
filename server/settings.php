1fb236547be2be54982cd172b828829cc3c0bbb3
devAxeon <dev@axeon.fr>
2021-07-23 15:36:41 +0200

    Security Ajax & SMTP

--- server/settings.php
+++ server/settings.php
@@ -2,11 +2,12 @@
 
 const SMTP_HOST = "SSL0.OVH.NET";
 const SMTP_USER = "noreply@coteaux-abbaye-caen.fr";
-const SMTP_PASS = "C8So0qwbfqi3HCPGvnLY";
+const SMTP_PASS = "qfGrEWEIST2\$bfevy81O";
 const SMTP_PORT = 465;
 const SMTP_ENCRYPT = 'ssl';
 
 const MAIL_RECEIVER = "formulaire.Caen_ca@marketing-lab.com";
+//const MAIL_RECEIVER = "sebastien.montlibert@axeon-software.com";
 const SEND_COPY_TO = null;
 
 CONST MANDATORY_FIELDS = array(
